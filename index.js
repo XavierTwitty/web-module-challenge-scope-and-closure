3ddec711da4a8b15f9b9ab56952b17cbc62bbb02
XavierTwitty <x.twitty@gmail.com>
2020-09-04 19:16:56 -0400

    commit for repo

--- index.js
+++ index.js
@@ -2,47 +2,67 @@
 
 /**
  * ### Challenge `processFirstItem`
- * 
+ *
  * @instructions
  * Implement a higher-order function called `processFirstItem`.
  * It takes two arguments:
  * @param stringList an array of strings.
  * @param callback function that takes a string as its argument.
  * @returns the result of invoking `callback` with the FIRST element in `stringList`.
- * 
+ *
  * Example of usage of this higher-order function:
  * Invoking `processFirstItem` passing `['foo', 'bar']` and `(str) => str + str`,
  * should return 'foofoo'.
-*/
+ */
 function processFirstItem(stringList, callback) {
-  return callback(stringList[0])
+  return callback(stringList[0]);
 }
 
 // ⭐️ Example Challenge END ⭐️
-
 
 ///// M V P ///////
 
 /* Task 1: `counterMaker`
  * Study the code for counter1 and counter2. Answer the questions below.
  * 
- * 1. What is the difference between counter1 and counter2?
+ * 1. What is the difference between counter1 and counter2? 
+ * 
+ *the variable countaining the number is in global scope in counter1  
+
+counter 1 inside the function there is closure 
+counter 2 count is in global scope 
+
  * 
  * 2. Which of the two uses a closure? How can you tell?
  * 
+ * counter1 uses closure because it was a block level funtion inside of another funtion.
+ * 
  * 3. In what scenario would the counter1 code be preferable? In what scenario would counter2 be better? 
  *
+ * counter1 is protected, if you want to reuse the same code 
+ * counter2 can be accesed to multiple things
 */
 
 // counter1 code
 function counterMaker() {
   let count = 0;
   return function counter() {
-   return count++;
-  }
+    return count++;
+  };
 }
 
 const counter1 = counterMaker();
+
+counter1();
+counter1();
+counter1();
+counter1();
+counter1();
+counter1();
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
+console.log(counter1());
 
 // counter2 code
 let count = 0;
@@ -51,22 +71,25 @@ function counter2() {
   return count++;
 }
 
+counter2();
+counter2();
 
+console.log(count);
 /* Task 2: inning() 
 
 Write a function called `inning` that returns a random number of points that a team scored in an inning. This should be a whole number between 0 and 2. */
 
-function inning(/*Code Here*/){
-
-    /*Code Here*/
-
+function inning(/*Code Here*/) {
+  return Math.floor(Math.random() * 2 + 0);
+  /*Code Here*/
 }
-
+console.log(inning());
 /* Task 3: finalScore()
 
 Write a higher order function called `finalScore` that accepts the callback function `inning` (from above) and a number of innings and and returns the final score of the game in the form of an object.
 
 For example, 
+
 
 finalScore(inning, 9) might return: 
 {
@@ -74,14 +97,25 @@ finalScore(inning, 9) might return:
   "Away": 5,
 }
 
-*/ 
+*/
 
-function finalScore(/*code Here*/){
+function finalScore(inning, inningNum) {
+  let home = 0;
+  let away = 0;
 
-  /*Code Here*/
-
+  for (let i = 0; i < inningNum; i++) {
+    home += inning();
+    away += inning();
+  }
+  return {
+    Home: home,
+    Away: away,
+  };
 }
 
+/*Code Here*/
+
+console.log(finalScore(inning, 9));
 /* Task 4: 
 
 Create a function called `scoreboard` that accepts the following parameters: 
@@ -103,8 +137,52 @@ and returns the score at each pont in the game, like so:
 
 Final Score: 6 - 10 */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
-}
+// function scoreboard(getInningScoreCB , inningCB , innings) {
 
+//  const scoreByInning = [];
+//  const temp = getInningScoreCB(inningCB);
+//  let suffix = '';
+//  let home = 0 ;
+//  let away = 0;
 
+//  for (let i = 0 ; i < innings ; i++){
+//     scoreByInning.push(temp());
+
+//     if ( i === 1 ){
+//       suffix = 'st'
+//     }
+//     if ( i === 2 ){
+//       suffix = 'nd'
+//     }
+//     if ( i === 3 ){
+//       suffix = 'rd'
+//     }
+//     else {
+//       suffix = 'th'
+//     }
+//     }
+//     console.log(`[i]${suffix} inning: ${home} - ${away} `)
+//     console.log (`Final Score: ${home} - ${away} `)
+//  }
+
+//   /* CODE HERE */
+
+// function getInningScore(inningCB){
+
+//   let home = 0 ;
+//   let away = 0;
+//   return function(){
+
+//     home += inningCB();
+//     away += inningCB();
+//       return {
+//         Home: home,
+//         Away: away,
+//       };
+
+//   }
+
+// }
+
+// console.log(scoreboard(getInningScore, innings(), 9))
+// console.log()
